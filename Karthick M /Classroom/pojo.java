15b148dfd63eb856cdafb9130085a5b9c4c52e1a
k-a-r-t-h-i-c-k <140366796+k-a-r-t-h-i-c-k@users.noreply.github.com>
2023-07-26 18:35:30 +0530

    Add files via upload

---
+++ Karthick M /Classroom/pojo.java
@@ -0,0 +1,40 @@
+package a;
+public class Employee
+{
+private String name;
+private String id;
+private String salary;
+
+public String getname()
+{
+return name;
+}
+
+public void setname(String name)
+{
+this.name=id;
+}
+
+public String getid()
+{
+return id;
+}
+
+public void setid(String id)
+{
+this.id=id;
+}
+
+public String getsal()
+{
+return sal;
+}
+
+public void setsal(String sal)
+{
+this.sal=sal;
+}
+}
+
+
+
---
+++ Karthick M /Classroom/pojo1.java
@@ -0,0 +1,14 @@
+package a;
+public class Main
+{
+public static void main(String a[])
+{
+ Employee e=new Employee();
+ e.setname("sree");
+ e.setid("1");
+ e.setsal("200000");
+ System.out.println("Name:"+e.getname());
+ System.out.println("ID:"+e.getid());
+ System.out.println("Salary:"+e.getsal());
+}
+}
